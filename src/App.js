294dcf051678e4e889169f899b1a6127490eaa2c
nathanielgrossman <nategrossman@mac.com>
2018-08-20 16:59:20 -0700

    basic setup for parcel and react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <span>hi</span>
+      </div>
+    );
+  }
+}
+
+export default App; 
